06d7c8b77e3b3d7a1a1de122c834affe3f69598e
Chris Anderson <chris@fauna.com>
2018-08-24 14:02:43 -0700

    query fauna from js function

--- src/js/get-query/index.js
+++ src/js/get-query/index.js
@@ -1,14 +1,25 @@
 let begin = require('@architect/functions')
+let faunadb = require('faunadb')
+
+let client = new faunadb.Client({
+  secret: process.env.FAUNA_SERVER_SECRET
+})
+
 
 function route (req, res) {
   let request = JSON.stringify(req, null, 2)
 
   // TODO change defaultJS
-  let defaultJS = `console.log('Hello frontend! ', ${request})`
 
   console.log('Hello backend! ', request)
-  res({
-    js: defaultJS
+
+  client.query("Hello from FaunaDB").then((faunaResult)=> {
+    let defaultJS = `console.log('${faunaResult} ', ${request})`
+    res({
+      js: defaultJS
+    })
+  }).catch((error) => {
+    console.log("backend error", error)
   })
 }
 
